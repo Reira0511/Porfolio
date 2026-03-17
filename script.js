46cb9d8d413af60c5ab8d6486d48109ad4a057d7
Reira0511 <reira55ikenaga@gmail.com>
2026-03-17 17:08:13 +1100

    my contact section added, some parts removed

--- script.js
+++ script.js
@@ -5,33 +5,3 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
-
-
-const contactForm = document.getElementById('contact-form');
-
-contactForm.addEventListener('submit', function(event) {
-  event.preventDefault(); 
-
-  const formData = new FormData(contactForm);
-  const data = Object.fromEntries(formData.entries()); 
-
-  fetch(contactForm.action, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json' 
-    },
-    body: JSON.stringify(data)
-  })
-  .then(response => {
-    if (response.ok) {
-      alert('Message sent successfully!');
-      contactForm.reset(); 
-    } else {
-      alert('There was a problem sending your message.');
-    }
-  })
-  .catch(error => {
-    alert('There was a network error. Please try again.');
-  });
-});
